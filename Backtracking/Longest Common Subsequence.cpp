3b33b538d5fed92a0a9941dd3404b2c9504d8516
NaveenE14 <107349514+NaveenE14@users.noreply.github.com>
2024-01-10 00:14:05 +0530

    Create Longest Common Subsequence.cpp

---
+++ Backtracking/Longest Common Subsequence.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int longestCommonSubsequence(string a, string b){
+        vector<vector<int>>dp(a.size()+1,vector<int>(b.size()+1,0));
+        for(int i=1;i<dp.size();i++){
+            for(int j=1;j<dp[0].size();j++){
+                if(a[i-1]==b[j-1]){
+                    dp[i][j]=1+dp[i-1][j-1];
+                }
+                else{
+                    dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+                }
+            }
+        }
+        string s="";
+        int i=dp.size()-1,j=dp[0].size()-1;
+        while(i>0 && j>0){
+            if(a[i-1]==b[j-1]){
+                s+=a[i-1];7
+            }
+            else if(dp[i-1][j] > dp[i][j-1]){
+                s+=a[i-1];
+                i++;
+            }
+            else{
+                s+=b[j-1];
+                j--;
+            }
+        }
+        cout<<s<<endl;
+        return dp[a.size()][b.size()];
+    }
+};
