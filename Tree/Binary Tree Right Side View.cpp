e362cf04cb00619dc28b486b8f274b0d25b74edd
NaveenE14 <107349514+NaveenE14@users.noreply.github.com>
2024-01-15 22:29:20 +0530

    Update Binary Tree Right Side View.cpp

--- Tree/Binary Tree Right Side View.cpp
+++ Tree/Binary Tree Right Side View.cpp
@@ -17,3 +17,27 @@ public:
         
     }
 };
+
+// BFS queue
+class Solution {
+public:
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int>ans;
+        if(root==NULL) return ans;
+        queue<TreeNode*>q;
+        q.push(root);
+        while(!q.empty()){
+           int n=q.size();
+           for(int i=0;i<n;i++){
+               TreeNode* a=q.front();
+               q.pop();
+               if(i==0){
+                   ans.push_back(a->val);
+               }
+               if(a->right!=NULL) q.push(a->right);
+               if(a->left!=NULL ) q.push(a->left);
+           }
+        }
+        return ans;
+    }
+};
