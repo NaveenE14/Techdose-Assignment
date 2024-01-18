6be7b7d9f56b60df829c5593bc95155a180faeaa
NaveenE14 <107349514+NaveenE14@users.noreply.github.com>
2024-01-18 19:23:12 +0530

    Create postorder.cpp

---
+++ Tree/postorder.cpp
@@ -0,0 +1,45 @@
+/*  class Solution {
+    void solve(TreeNode* root, vector<int>&ans){
+        if(root==NULL) 
+             return;
+        solve(root->left,ans);
+        solve(root->right,ans);
+        ans.push_back(root->val);
+    }
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        vector<int>ans;
+        solve(root,ans);
+        return ans;
+
+    }
+}; 
+
+// iterative 2 stack 
+class Solution {
+public:
+    vector<int> postorderTraversal(TreeNode* root) {
+        stack<TreeNode*> st1,st2;
+        vector<int>ans;
+        if( root  ==  NULL ) return ans;
+        st1.push(root);
+        while(!st1.empty()){
+            root=st1.top();
+            st1.pop();
+            st2.push(root);
+            if(root->left!= NULL){
+                st1.push(root->left);
+            }
+            if(root->right != NULL) {
+                st1.push(root->right);
+            }
+            
+        }
+        while(!st2.empty()){
+            ans.push_back(st2.top()->val);
+            st2.pop();
+        }
+        return ans;
+    }
+};
+
