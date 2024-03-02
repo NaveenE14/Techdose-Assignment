52a3c9ec54d63d5f3d31a2052bbd510a9edaed57
NaveenE14 <107349514+NaveenE14@users.noreply.github.com>
2024-03-02 12:21:44 +0530

    Update Subarray Sum Equals K.cpp

--- HashMap/Subarray Sum Equals K.cpp
+++ HashMap/Subarray Sum Equals K.cpp
@@ -1,15 +1,17 @@
 class Solution {
 public:
     int subarraySum(vector<int>& nums, int k) {
-        unordered_map<int,int>m;
-        m[0]=1;
-        int pre=0,c=0;
-        for(int i=0;i<nums.size();i++) {
-             pre+=nums[i];
-             int rem=pre-k;
-             c+=m[rem];
-             m[pre]++;
+        map<int,int>mp;
+        int ans=0,sum=0;
+        for(int i=0;i<nums.size();i++){
+            sum+=nums[i];
+            if(sum==k){
+                ans++;
+            }
+            int rem=sum-k;
+            ans+=mp[rem];
+            mp[sum]++;
         }
-        return c;
+        return ans;
     }
 };
